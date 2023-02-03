5314055ce09fc7423454174d23568e0adff14158
nizamov0902 <nizamoff.rus@gmail.com>
2023-02-03 11:57:45 +0100

    add basic CRUD methods

--- script.ts
+++ script.ts
@@ -3,7 +3,49 @@ const prisma = new PrismaClient()
 
 
 async function main() {
-  await prisma.user.deleteMany()
+
+    // Create a new user
+    const user = await prisma.user.create({
+        data: {
+            age: 30,
+            name: "Clark",
+            email: "klark@test.com",
+        }
+      }
+    )
+
+
+// Find user
+  // @ts-ignore
+    //const user = await prisma.user.findMany({
+     //  where: {
+      //    name: {not: "Sally"},
+      //  },
+      //  orderBy: {
+       //    age: "asc",
+      //  }
+   // })
+
+
+    // Update user
+    //const user = await prisma.user.update({
+      //  where: {
+         //   email: "sally@test.com"
+       // },
+      //  data: {
+      ////      email: "sally5@test.com"
+        //},
+    //})
+
+
+    //Delete user
+   // const user = await prisma.user.delete({
+        //where: {
+            //email: "test2@gmail"
+        //}
+    //})
+
+    console.log(user)
 }
 
 main()
